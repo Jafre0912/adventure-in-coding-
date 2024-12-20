c7ff3fd9eb547ae24928131c631830651d6d1555
jafre0912 <jafre0912@gmail.com>
2024-12-20 11:36:26 +0530

    Alletec Technical Test Questions

---
+++ RepeatedWfinder.java
@@ -0,0 +1,34 @@
+package Alletec;
+import java.util.*;
+
+public class RepeatedWfinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String s = sc.nextLine();
+        
+        s = s.toLowerCase().replaceAll("[^a-zA-Z ]", "");
+        String[] w = s.split("\\s+");
+        
+        Map<String, Integer> wc = new HashMap<>();
+        
+        for (String word : w) {
+            wc.put(word, wc.getOrDefault(word, 0) + 1);
+        }
+        
+        System.out.println("Repeated words and their occurrences:");
+        boolean f = false;
+        for (Map.Entry<String, Integer> e : wc.entrySet()) {
+            if (e.getValue() > 1) {
+                System.out.println(e.getKey() + ": " + e.getValue());
+                f = true;
+            }
+        }
+        
+        if (!f) {
+            System.out.println("No repeated words found.");
+        }
+
+        sc.close();
+    }
+}
